053c93734c49d508f033b9f27baa62aa420ec094
Joseba S <jjoseba@gmail.com>
2017-09-04 13:07:22 +0200

    Campos de evento opcionales

--- bands/models/event.py
+++ bands/models/event.py
@@ -19,12 +19,12 @@ class Event(models.Model):
     day = models.DateField(null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
     duration = models.IntegerField(null=True, blank=True, default=60)
-    created_by = models.ForeignKey(User, null=True, verbose_name='Creado por')
+    created_by = models.ForeignKey(User, null=True, blank=True, verbose_name='Creado por')
     event_uid = models.UUIDField(default=uuid.uuid4, editable=False)
     title = models.TextField(null=True, blank=True, verbose_name='Título del evento')
     poster = models.ImageField(null=True, blank=True, upload_to=poster_path, verbose_name='Imagen del evento')
     price = models.FloatField(null=True, blank=True, verbose_name='Precio')
-    ticketLink = models.TextField(null=True, verbose_name='Enlace compra de entradas')
+    ticketLink = models.TextField(null=True, blank=True, verbose_name='Enlace compra de entradas')
 
     class Meta:
         verbose_name = 'Concierto'
