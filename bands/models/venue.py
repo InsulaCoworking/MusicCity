8cf750e24864e05827f1db57c500f0d363d6f1f1
Joseba S <jjoseba@gmail.com>
2017-09-05 16:54:37 +0200

    Añadida imagen de perfil a escenarios

--- bands/models/venue.py
+++ bands/models/venue.py
@@ -15,9 +15,16 @@ class Venue(models.Model):
     description = models.TextField(null=False, blank=True)
     latitude = models.FloatField(null=False)
     longitude = models.FloatField(null=False)
-    image = ProcessedImageField(null=True, blank=True, upload_to=RandomFileName('venue/'),
+    image = ProcessedImageField(null=True, blank=True, upload_to=RandomFileName('venue/'), verbose_name='Imagen de cabecera',
                                  processors=[ResizeToFit(900, 900, upscale=False)], format='JPEG')
+    profile_image = ProcessedImageField(null=True, blank=True, upload_to=RandomFileName('venue/'),
+                                verbose_name='Imagen de perfil',
+                                processors=[ResizeToFit(512, 512, upscale=False)], format='JPEG')
     address = models.TextField(null=True, blank=True)
+
+    facebook_link = models.CharField(null=True, blank=True, verbose_name='Página de Facebook', max_length=250)
+    twitter_link = models.CharField(null=True, blank=True, verbose_name='Perfil de Twitter', max_length=250)
+    webpage_link = models.CharField(null=True, blank=True, verbose_name='Página web', max_length=250)
 
     class Meta:
         verbose_name = 'Escenario'
