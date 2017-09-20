28cfd07456da2fc225938ece414ddb6909b8ba59
Joseba S <jjoseba@gmail.com>
2017-09-20 13:54:08 +0200

    Añadidos enlaces para compartir en redes sociales

--- bands/static/js/scripts.js
+++ bands/static/js/scripts.js
@@ -1,4 +1,29 @@
 
+function popUp(url) {
+
+    if (url == null) return;
+    console.log(url);
+    var w = 490;
+    var h = 350;
+
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;
+    var newWindow = window.open(url, '', 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    if (window.focus) {
+        newWindow.focus();
+    }
+	return false;
+}
+
 function loadResults(resultsContainer, url){
     if (url == null || url == '' || url.startsWith('#')){
         return;
@@ -34,7 +59,12 @@ $(document).ready(function() {
 
     var searchForm = $('#searchForm');
     var url = searchForm.attr('action');
-    var results = $("#results")
+    var results = $("#results");
+
+    $('a.popup-window').on('click', function(e){
+        e.preventDefault();
+        popUp(this.href);
+    });
 
  /* $(".select2").on('change',function() {
     data = searchForm.serialize();
