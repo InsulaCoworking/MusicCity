47507cda26f7d37444eda6aef68e9f6bdfb2d294
Joseba S <jjoseba@gmail.com>
2017-11-08 17:00:24 +0100

    Añadida opción para añadir datos de sala para conciertos de fuera de la ciudad

--- bands/forms/event.py
+++ bands/forms/event.py
@@ -1,15 +1,14 @@
 # coding=utf-8
 from django import forms
-from django.db.models import BLANK_CHOICE_DASH
 
-from bands.models import Venue, Event
+from bands.models import Event
 
 
 class EventForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', '')
-        form = super(EventForm, self).__init__(*args, **kwargs)
+        super(EventForm, self).__init__(*args, **kwargs)
 
     event_bands = forms.CharField(widget=forms.HiddenInput(), required=False)
 
@@ -18,7 +17,7 @@ class EventForm(forms.ModelForm):
         exclude = ['created_by']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows':6}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 6}),
             'venue_address': forms.TextInput(attrs={'class': 'form-control'}),
             'venue_name': forms.TextInput(attrs={'class': 'form-control'}),
 
@@ -29,8 +28,15 @@ class EventForm(forms.ModelForm):
             'day': forms.DateInput(attrs={'class': 'form-control'}),
             'time': forms.TimeInput(attrs={'class': 'form-control'}),
 
-            'price': forms.NumberInput(attrs={'class': 'form-control text-right', 'placeholder':0.00}),
-            'price_preorder': forms.NumberInput(attrs={'class': 'form-control text-right', 'placeholder':0.00}),
+            'price': forms.NumberInput(attrs={'class': 'form-control text-right', 'placeholder': 0.00}),
+            'price_preorder': forms.NumberInput(attrs={'class': 'form-control text-right', 'placeholder': 0.00}),
             'ticket_link': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
+    def clean(self):
+        cleaned_data = super(EventForm, self).clean()
+        if 'venue' in cleaned_data and cleaned_data['venue']:
+            cleaned_data['venue_name'] = ''
+            cleaned_data['venue_address'] = ''
+
+        return cleaned_data
