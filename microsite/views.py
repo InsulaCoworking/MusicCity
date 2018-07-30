c66ed3b200f6e225aa0a855f8eb1701ffe3d30a4
Joseba S <jjoseba@gmail.com>
2018-07-30 17:47:17 +0200

    Show latest news in microsite with related tag

--- microsite/views.py
+++ microsite/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import get_object_or_404, render
 
 from bands.models import Band
 from microsite.models import Microsite
-
+from zinnia.models.entry import Entry
 
 def microsite_index(request, slug):
     microsite = get_object_or_404(Microsite, slug=slug)
 
     bands = Band.objects.filter(events__id__in=microsite.events.all()).distinct()
+    entries = Entry.published.filter(tags__icontains=microsite.news_tag)
+
     return render(request, 'microsite/index.html', {
         'microsite': microsite,
         'bands':bands,
+        'entries': entries,
     })
