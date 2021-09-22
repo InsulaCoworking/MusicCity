a7e7b7a3fe86f49bf1c28cf54df3c7f2904c0977
Joseba S <jjoseba@gmail.com>
2021-09-22 12:14:37 +0200

    Added password reset form

--- bands/urls.py
+++ bands/urls.py
@@ -46,6 +46,12 @@ urlpatterns = [
     url(r'^logout/$',  auth_views.LogoutView.as_view(), name='logout'),
     url(r'^register/$', views.register, name='register'),
 
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
     url(r'^dashboard/$', views.profile, name='dashboard'),
     url(r'^profile/edit$', views.edit_profile, name='edit_profile'),
     url(r'^profile/edit/password$', views.profile_save_password, name='profile_save_password'),
