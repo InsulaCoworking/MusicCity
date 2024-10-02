84931e971b6a2f71e0208a0e8bf1ec528c929b17
Joseba S <jjoseba@gmail.com>
2024-10-02 12:03:36 +0200

    Updated ga configuration

--- helpers/templatetags/settings_value.py
+++ helpers/templatetags/settings_value.py
@@ -4,7 +4,7 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-ALLOWABLE_VALUES = ("GMAPS_APIKEY", "MAIN_PAGE_TITLE", "INITIAL_LATITUDE", "INITIAL_LONGITUDE", "BASESITE_URL", "INLINE_INPUT_SEPARATOR")
+ALLOWABLE_VALUES = ("GMAPS_APIKEY", "ANALYTICS_KEY", "MAIN_PAGE_TITLE", "INITIAL_LATITUDE", "INITIAL_LONGITUDE", "BASESITE_URL", "INLINE_INPUT_SEPARATOR")
 
 # settings value (based on https://stackoverflow.com/a/21593607)
 @register.simple_tag
